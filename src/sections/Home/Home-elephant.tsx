2f95fba974be309acd9a8372037d70f094d7361c
saigyou_ayakashi <minuit1993@live.fr>
2023-08-24 02:17:16 +0200

    update

--- src/sections/Home/Home-elephant.tsx
+++ src/sections/Home/Home-elephant.tsx
@@ -4,6 +4,12 @@ import elephant from "@assets/lotties/elephant.json";
 const HomeElephant = () => {
   return (
     <>
+      {/*
+        Nellie the elephant packed her trunk and
+        said goodbye to the circus
+        Off she went with a trumpety-trump
+        Trump, trump, trump
+      */}
       <Lottie animationData={elephant} />
     </>
   );
