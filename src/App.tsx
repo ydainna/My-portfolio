f4fab8931b33a76af3dd4b101b82e7b06bd8f661
ydainna <minuit1993@live.fr>
2024-03-22 00:02:30 +0100

    Add ASCII art and lyrics to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,29 @@ import Router from "./components/Router";
 const App = () => {
   return (
     <>
+      {
+        console.log(`
+        __                 
+        '. \\  🅷🅴🅻🅻🅾, 🅼🆈 🅽🅰🅼🅴 🅸🆂 🅴🅻🅼🅴🆁 ❗
+         '- \\               
+          / /_         .---.
+         / | \\\,.\\/--.//    )
+         |  \\//        )/  / 
+          \\  ' ^ ^    /    )____.----..  6
+           '.____.    .___/            \\._) 
+              .\\/\\.                      )
+               '\\                       /
+               _/ \\/    ).        )    (
+              /#  .!    |        /\\    /
+              \\  C// #  /'-----''/ #  / 
+           .   'C/ |    |    |   |    |  ,
+           \\), .. .'OOO-'. ..'OOO'OOO-'. ..\\(,
+        Nellie the elephant packed her trunk and 🐘
+        said goodbye to the circus 🎪
+        Off she went with a trumpety-trump 🎺
+        Trump, trump, trump 🎶🎵
+        `)
+      }
       <Router />
     </>
   );
