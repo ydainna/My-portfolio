87c166d698197afeb831ac9a0b32297b8488bc9a
ydainna <minuit1993@live.fr>
2024-03-22 11:07:39 +0100

    Remove commented code and unused import statements

--- src/sections/Home/Home-elephant.tsx
+++ src/sections/Home/Home-elephant.tsx
@@ -4,12 +4,6 @@ import elephant from "@assets/lotties/elephant.json";
 const HomeElephant = () => {
   return (
     <>
-      {/*
-        Nellie the elephant packed her trunk and
-        said goodbye to the circus
-        Off she went with a trumpety-trump
-        Trump, trump, trump
-      */}
       <Lottie animationData={elephant} />
     </>
   );
